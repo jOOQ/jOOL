9382fae360e81c6c69c991d21ea1163176ff5d00
lukaseder <lukas.eder@gmail.com>
2014-09-03 10:03:14 +0200

    [#6] Add Streams.zip(Stream<T1>, Stream<T2>)

---
+++ src/test/java/org/jooq/lambda/StreamsTest.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2009-2014, Data Geekery GmbH (http://www.datageekery.com)
+ * All rights reserved.
+ *
+ * This work is dual-licensed
+ * - under the Apache Software License 2.0 (the "ASL")
+ * - under the jOOQ License and Maintenance Agreement (the "jOOQ License")
+ * =============================================================================
+ * You may choose which license applies to you:
+ *
+ * - If you're using this work with Open Source databases, you may choose
+ *   either ASL or jOOQ License.
+ * - If you're using this work with at least one commercial database, you must
+ *   choose jOOQ License
+ *
+ * For more information, please visit http://www.jooq.org/licenses
+ *
+ * Apache Software License 2.0:
+ * -----------------------------------------------------------------------------
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * jOOQ License and Maintenance Agreement:
+ * -----------------------------------------------------------------------------
+ * Data Geekery grants the Customer the non-exclusive, timely limited and
+ * non-transferable license to install and use the Software under the terms of
+ * the jOOQ License and Maintenance Agreement.
+ *
+ * This library is distributed with a LIMITED WARRANTY. See the jOOQ License
+ * and Maintenance Agreement for more details: http://www.jooq.org/licensing
+ */
+package org.jooq.lambda;
+
+import org.jooq.lambda.tuple.Tuple2;
+import org.junit.Test;
+
+import java.sql.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Lukas Eder
+ */
+public class StreamsTest {
+
+    @Test
+    public void testZipEqualLength() {
+        List<Tuple2<Integer, String>> list = Streams.zip(
+            Stream.of(1, 2, 3),
+            Stream.of("a", "b", "c")
+        ).collect(Collectors.toList());
+
+        assertEquals(3, list.size());
+        assertEquals(1, (int) list.get(0).v1);
+        assertEquals(2, (int) list.get(1).v1);
+        assertEquals(3, (int) list.get(2).v1);
+        assertEquals("a", list.get(0).v2);
+        assertEquals("b", list.get(1).v2);
+        assertEquals("c", list.get(2).v2);
+    }
+
+    @Test
+    public void testZipDifferingLength() {
+        List<Tuple2<Integer, String>> list = Streams.zip(
+            Stream.of(1, 2),
+            Stream.of("a", "b", "c", "d")
+        ).collect(Collectors.toList());
+
+        assertEquals(2, list.size());
+        assertEquals(1, (int) list.get(0).v1);
+        assertEquals(2, (int) list.get(1).v1);
+        assertEquals("a", list.get(0).v2);
+        assertEquals("b", list.get(1).v2);
+    }
+}
