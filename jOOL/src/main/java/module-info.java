bb6410b8f30142e3bd793a44bc647b90d439a6d2
lukaseder <lukas.eder@gmail.com>
2018-06-12 14:50:21 +0200

    [#335] Missing exports section in module-info.java

--- jOOL/src/main/java/module-info.java
+++ jOOL/src/main/java/module-info.java
@@ -1,5 +1,12 @@
 /* [java-9] */
 module org.jooq.jool {
-
+    exports org.jooq.lambda;
+    exports org.jooq.lambda.exception;
+    exports org.jooq.lambda.fi.lang;
+    exports org.jooq.lambda.fi.util;
+    exports org.jooq.lambda.fi.util.concurrent;
+    exports org.jooq.lambda.fi.util.function;
+    exports org.jooq.lambda.function;
+    exports org.jooq.lambda.tuple;
 }
 /* [/java-9] */
